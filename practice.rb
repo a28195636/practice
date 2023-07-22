972a79f8ca53b7ff77b12efbdad8a69a528c40ab
Akira Suzuki <a28195636@gmail.com>
2023-07-22 09:44:54 +0000

    Ruby 03 4

--- practice.rb
+++ practice.rb
@@ -1,5 +1,13 @@
-fruits = ["りんご","みかん","ぶどう","バナナ","パイナップル"]
+=begin
+  for文の始めの値を定義する
+=end
+start = 1
+# for文の終わりの値を定義する
+end_num = 100
 
-fruits.each do |fruits|
-  puts fruits
+(start..end_num).each do |i|
+  # 5で割り切れたら{}内を実行する
+  if i % 5 == 0
+    puts i
+  end
 end
