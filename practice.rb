7aa9607007c7a22f6327b9e03643d7b7caadb6af
Akira Suzuki <a28195636@gmail.com>
2023-07-22 10:11:34 +0000

    Ruby 04 4

--- practice.rb
+++ practice.rb
@@ -1,11 +1,18 @@
 arr = [1, 3, 5 ,7, 9]
 
-def kakezan(arr)
-  result = 1
-  arr.each do |arr|
-  result *= arr
+def max_array(arr)
+  # とりあえず配列の最初の要素を一番大きい値とする
+  max_number = arr[0]
+
+  arr.each do |a|
+    if max_number < a
+      max_number = a
+    end
   end
-  puts result
+
+  return max_number
 end
 
-kakezan(arr)
+
+result = max_array(arr)
+puts result
