06d3e4bbfc9d4a31cad58692e11388954bf9e284
Akira Suzuki <a28195636@gmail.com>
2023-07-22 08:28:25 +0000

    Ruby 01 3

--- practice.rb
+++ practice.rb
@@ -1,3 +1,5 @@
-array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"]
+hello = "Hello, "
+name = "Suzuki"
+world = "'s World!"
 
-puts array_month[7]
+puts hello + name + world
