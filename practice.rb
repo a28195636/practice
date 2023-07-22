5538fdb6d0f93ad611bdb1c999588f59a87385ee
Akira Suzuki <a28195636@gmail.com>
2023-07-22 08:33:25 +0000

    Ruby 01 5

--- practice.rb
+++ practice.rb
@@ -1,4 +1,17 @@
-omusubi_tech = "omusubi"
-omusubi_tech << " tech"
+calendar_2023 = {
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+}
 
-puts omusubi_tech
+# 12月を表示する
+puts calendar_2023["December"]
