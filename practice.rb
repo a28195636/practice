d192f5e707104a9d0a629da6def788971551cd71
Akira Suzuki <a28195636@gmail.com>
2023-07-22 09:49:10 +0000

    Ruby 04 1

--- practice.rb
+++ practice.rb
@@ -1,13 +1,6 @@
-=begin
-  for文の始めの値を定義する
-=end
-start = 1
-# for文の終わりの値を定義する
-end_num = 100
-
-(start..end_num).each do |i|
-  # 5で割り切れたら{}内を実行する
-  if i % 5 == 0
-    puts i
-  end
+def nibai(i)
+  i *= 2
+  puts i
 end
+
+nibai(2)
